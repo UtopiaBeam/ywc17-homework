b964fab977a0c4702ad5b45e08dce6129a8bede2
UtopiaBeam <utopiabeam@gmail.com>
2019-10-31 11:50:23 +0700

    Updates

---
+++ precache-manifest.6676c5d469d2027a732fbec55a39b3d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0456b2b7c5d6c1eec77e8700c82dffae",
+    "url": "/UtopiaBeam/ywc17-homework/index.html"
+  },
+  {
+    "revision": "85c2318627fe9ab493ee",
+    "url": "/UtopiaBeam/ywc17-homework/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "26cb3938ec5305da0433",
+    "url": "/UtopiaBeam/ywc17-homework/static/css/main.463b3360.chunk.css"
+  },
+  {
+    "revision": "85c2318627fe9ab493ee",
+    "url": "/UtopiaBeam/ywc17-homework/static/js/2.0c174e8a.chunk.js"
+  },
+  {
+    "revision": "26cb3938ec5305da0433",
+    "url": "/UtopiaBeam/ywc17-homework/static/js/main.abb5eea3.chunk.js"
+  },
+  {
+    "revision": "30ff97a65cdb21ab3849",
+    "url": "/UtopiaBeam/ywc17-homework/static/js/runtime-main.d6734368.js"
+  },
+  {
+    "revision": "45415b3d8382c9a11e16af5f88efab0e",
+    "url": "/UtopiaBeam/ywc17-homework/static/media/TATSanaChon-Bold.45415b3d.woff2"
+  },
+  {
+    "revision": "29c3fec8f8a81165e4a924a4f50df8d9",
+    "url": "/UtopiaBeam/ywc17-homework/static/media/TATSanaChon-Regular.29c3fec8.woff2"
+  },
+  {
+    "revision": "f51f1b81ee331401e0118c8e97c18083",
+    "url": "/UtopiaBeam/ywc17-homework/static/media/TATSanaSuksa-Regular.f51f1b81.woff2"
+  }
+]);
