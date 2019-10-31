b64d33abd2b3b4c96f479293139199f892780a17
UtopiaBeam <utopiabeam@gmail.com>
2019-10-31 08:44:52 +0700

    :lipstick: Fix navbar link color when hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { NavbarItem } from '../services';
+import { BASE_URL } from '../const';
 import './Navbar.scss';
 
 interface Props {
@@ -18,12 +19,7 @@ export default ({ items }: Props) => {
     <>
       <Navbar expand="sm" fixed="top" bg="white" className="navbar">
         <div id="brand">
-          <img
-            alt="logo"
-            width="35px"
-            height="48px"
-            src="https://www.xn--b3caa1e2a7e2b0h2be.com/img/logo/footer.png"
-          />
+          <img alt="logo" width="35px" height="48px" src={`${BASE_URL}/img/logo/footer.png`} />
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">{navbarItems}</Navbar.Collapse>
