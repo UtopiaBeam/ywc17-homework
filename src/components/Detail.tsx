c4d69733723ffe345a404533824e809f714fb892
UtopiaBeam <utopiabeam@gmail.com>
2019-11-02 10:34:09 +0700

    :lock: Sanitize html before dangerouslyinnerhtml

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PanjsData } from '../services';
 import Container from 'react-bootstrap/Container';
+import createDOMPurify from 'dompurify';
 import './Detail.scss';
 
 interface Props {
@@ -8,6 +9,8 @@ interface Props {
 }
 
 export default ({ data }: Props) => {
+  const DOMPurify = createDOMPurify(window);
+
   return (
     <>
       <Container id="detail-wrapper">
@@ -16,10 +19,16 @@ export default ({ data }: Props) => {
           <br />
           ในประเทศ <span className="nowrap">"ชิมช้อปใช้"</span>
         </div>
-        <div className="content my-4" dangerouslySetInnerHTML={{ __html: data.detail }} />
+        <div
+          className="content my-4"
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.detail) }}
+        />
         <div>
           <div id="condition-title">เงื่อนไขการเข้าร่วมมาตรการ</div>
-          <div className="content mt-2" dangerouslySetInnerHTML={{ __html: data.condition }} />
+          <div
+            className="content mt-2"
+            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.condition) }}
+          />
         </div>
       </Container>
     </>
